e15f7ebadc2b4d3257d796d6f51bea9ab6a022b2
sawenchalmers <sawen@chalmers.se>
2024-07-09 15:05:48 +0200

    fix: merged files into one, fixed issues with Honeybee in Py3

--- src/heath.py
+++ src/heath.py
@@ -1,8 +1,18 @@
 from Grasshopper.Kernel import GH_RuntimeMessageLevel as Message
 import rhinoscriptsyntax as rs
+import os, sys
+import Rhino
+import scriptcontext as sc
+import importlib
 
 try:  # import the ladybug_rhino and honeybee dependencies
     from ladybug_rhino.config import angle_tolerance, tolerance
+    # MEGA HACK because something changed in the Rhino API rendering HB useless
+    # https://discourse.ladybug.tools/t/ladybug-modules-relying-on-rhino-geometry-collections-seem-not-to-work-in-rhino-8-python-3/25222
+    # from ladybug_rhino.togeometry import to_polyface3d
+    from patch_honeybee import to_polyface3d_patched
+    
+    importlib.reload(sys.modules["patch_honeybee"])
     from ladybug_rhino.grasshopper import document_counter
     from honeybee.room import Room
     from honeybee.typing import clean_and_id_string
@@ -10,6 +20,25 @@ try:  # import the ladybug_rhino and honeybee dependencies
     from ladybug_rhino.intersect import bounding_box, intersect_solids 
 except ImportError as e:
     raise ImportError('\nFailed to import ladybug_rhino:\n\t{}'.format(e))
+
+def get_results_folder(ghdoc):
+    sc.doc = Rhino.RhinoDoc.ActiveDoc
+
+    directory = os.path.dirname(str(Rhino.RhinoDoc.ActiveDoc.Path))
+    if directory:
+        mpl_folder = os.path.join(directory, heath_globals.results_folder)
+    else:
+        directory = (ghenv.Document.FilePath).split("\\")[:-1]
+        full_path = '\\'.join(directory)
+        mpl_folder = os.path.join(full_path, heath_globals.results_folder)
+
+    try:
+        os.makedirs(mpl_folder)
+    except:
+        pass
+
+    sc.doc = ghdoc
+    return mpl_folder
 
 def create_hb_model(room_geo, construction_sets):
     room_solids = _intersect_room_geometry(room_geo)
@@ -34,10 +63,10 @@ def _create_rooms(room_solids, names):
             names[i]        
         name = clean_and_id_string(display_name)
 
-        print(geo)
+
         # create the Room
         room = Room.from_polyface3d(
-            name, to_polyface3d(geo), roof_angle=roof_angle,
+            name, to_polyface3d_patched(geo), roof_angle=roof_angle,
             floor_angle=floor_angle, ground_depth=tolerance)
         room.display_name = display_name
 
@@ -48,18 +77,20 @@ def _create_rooms(room_solids, names):
                 'Preview the output Room to see the holes in your model.'
             print(msg)
             utils.warn(ghenv.Component, msg)
+        rooms.append(room)
     return rooms
 
 # hmm, don't like this function mutating the rooms, but it is the HB way
 def _apply_construction_sets(rooms, construction_sets):
-    for room, i in enumerate(rooms):
+    for i, room in enumerate(rooms):
         constr_set = construction_sets[i] \
             if utils.list_len_equal(construction_sets, rooms) \
             else construction_sets[0]
         room.properties.energy.construction_set = constr_set
 
 class heath_globals:
-    version = "0.2.0-dev"
+    version = "0.3.0-dev"
+    results_folder = "results"
 
 class utils:
     # not sure the "@staticmethod" thing is needed anymore in python 3
